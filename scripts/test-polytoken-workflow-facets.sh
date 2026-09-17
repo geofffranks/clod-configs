340cdfd79ba513dea12766bcf5e8c73cdb9a26fe
Geoff Franks <geoff.franks@gmail.com>
2026-09-17 02:42:24 +0000

    Align workflow harness with goal proposal tool

--- scripts/test-polytoken-workflow-facets.sh
+++ scripts/test-polytoken-workflow-facets.sh
@@ -519,11 +519,11 @@ run_delivery_policy() {
   expect_fm "workflow-project-manager: model pin" "$DELIVERY" '.polytoken.model' '"zai/glm-5.3-flash(high)"'
   expect_list "workflow-project-manager: fallback_models" "$DELIVERY" '.polytoken.fallback_models' "codex/gpt-5.6-luna-1m(medium)"
   expect_list "delivery: tools" "$DELIVERY" '.polytoken.tools' \
-    "file_read,file_write,file_edit_search_replace,glob,grep,lsp,shell_exec,shell_monitor,shell_service,subagent,message_subagent,skill,job_status,job_block,job_result,job_cancel,list_jobs,ask_user_question,tool_search,todo_create,todo_update,todo_complete,todo_delete,todo_list,pushd,popd,switch_facet,read_goal,complete_goal,block_goal,mcp__ratatoskr"
+    "file_read,file_write,file_edit_search_replace,glob,grep,lsp,shell_exec,shell_monitor,shell_service,subagent,message_subagent,skill,job_status,job_block,job_result,job_cancel,list_jobs,ask_user_question,tool_search,todo_create,todo_update,todo_complete,todo_delete,todo_list,pushd,popd,switch_facet,propose_goal,read_goal,complete_goal,block_goal,mcp__ratatoskr"
   expect_list "delivery: tools_deny" "$DELIVERY" '.polytoken.tools_deny' \
     "write_plan,edit_plan,handoff_plan"
   expect_list "delivery: undeferred_tools" "$DELIVERY" '.polytoken.undeferred_tools' \
-    "file_read,file_write,file_edit_search_replace,glob,grep,lsp,shell_exec,subagent,message_subagent,skill,job_status,job_block,job_result,list_jobs,ask_user_question,todo_create,todo_update,todo_complete,todo_list,read_goal,complete_goal,block_goal"
+    "file_read,file_write,file_edit_search_replace,glob,grep,lsp,shell_exec,subagent,message_subagent,skill,job_status,job_block,job_result,list_jobs,ask_user_question,todo_create,todo_update,todo_complete,todo_list,read_goal,complete_goal,block_goal,propose_goal"
   expect_list "delivery: skills_allow" "$DELIVERY" '.polytoken.skills_allow' \
     "tag!research,brainstorming,github-project-backlog,agent-orchestration,git-workflow,using-git-worktrees,systematic-debugging,test-driven-development,receiving-code-review,requesting-code-review,verification-before-completion,artifact-retention-policy,polytoken:modifying-polytoken,polytoken:researching-on-the-internet,polytoken:investigating-a-codebase"
   expect_fm "delivery: skills_deny empty" "$DELIVERY" '.polytoken.skills_deny' '[]'
